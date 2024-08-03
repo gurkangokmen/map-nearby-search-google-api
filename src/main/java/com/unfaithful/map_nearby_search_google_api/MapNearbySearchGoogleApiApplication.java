bdb5048462a8f2a714bfffc2ac7177de925ea72e
Gürkan Gökmen <122023578+gurkangokmen@users.noreply.github.com>
2024-08-03 12:28:50 +0300

    Initialized map-nearby-search-google-api project with eureka properties.

---
+++ src/main/java/com/unfaithful/map_nearby_search_google_api/MapNearbySearchGoogleApiApplication.java
@@ -0,0 +1,13 @@
+package com.unfaithful.map_nearby_search_google_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapNearbySearchGoogleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapNearbySearchGoogleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unfaithful/map_nearby_search_google_api/MapNearbySearchGoogleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unfaithful.map_nearby_search_google_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapNearbySearchGoogleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
